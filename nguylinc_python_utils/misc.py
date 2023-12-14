a1920de4e46ad3067abcfab13c44cc9d87d2a4ce
Lincoln Nguyen <137611486+rinkaaan@users.noreply.github.com>
2023-12-13 22:11:52 -0800

    Add rename_substring_in_files, get_free_port

--- nguylinc_python_utils/misc.py
+++ nguylinc_python_utils/misc.py
@@ -16,3 +16,41 @@ def rename_at_root(root, old, new):
 def hello_world():
     print("Hello World!")
     return "Hello World!"
+
+
+def rename_substring_in_files(root_dir, old_substring, new_substring, file_extensions=None):
+    for root, dirs, files in os.walk(root_dir):
+        if ".git" in root:
+            continue
+
+        for file in files:
+            # Check if file ends in any of the file extensions
+            matches_extension = False
+            if file_extensions:
+                for extension in file_extensions:
+                    if file.endswith(extension):
+                        matches_extension = True
+                        break
+            if file_extensions and not matches_extension:
+                continue
+
+            # Rename files
+            new_filename = file.replace(old_substring, new_substring)
+            rename_at_root(root, file, new_filename)
+
+            with open(os.path.join(root, new_filename), "r") as f:
+                contents = f.read()
+                new_contents = contents.replace(old_substring, new_substring)
+                with open(os.path.join(root, new_filename), "w") as f2:
+                    f2.write(new_contents)
+
+    # Renaming directories remains the same (not affected by file_regexes)
+    for root, dirs, files in os.walk(root_dir):
+        if ".git" in root:
+            continue
+
+        for directory in dirs:
+            # Rename directories
+            new_directory = directory.replace(old_substring, new_substring)
+            rename_at_root(root, directory, new_directory)
+
